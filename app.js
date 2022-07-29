b13dde0d12574323fc572cede970e455d7cd4843
Steven Lo <steven.lo@baicommunications.com.hk>
2022-07-29 12:48:16 +0800

    revised newline

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-message = "Welcome to AWS Devops learning path!\r\n";
-message += "Build Attempt = 2";
+message = "Welcome to AWS Devops learning path!\n";
+message += "Build Attempt = 3";
 
 app.get('/', (req, res) => res.send(message));
 
